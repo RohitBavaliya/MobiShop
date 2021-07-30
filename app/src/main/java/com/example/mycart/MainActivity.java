b1b98057491c7e990a10fc102550b1aa2b7d2473
rohit <rohit13bavaliya@gmail.com>
2021-07-30 13:06:53 +0530

    Login

--- app/src/main/java/com/example/mycart/MainActivity.java
+++ app/src/main/java/com/example/mycart/MainActivity.java
@@ -8,8 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     TextView go_register;
@@ -45,5 +48,33 @@ public class MainActivity extends AppCompatActivity {
     private void process_login(String email, String password)
     {
         Call<ResponseModel> call = ApiController.getInstance().getApi().getLogin(email,password);
+
+        call.enqueue(new Callback<ResponseModel>() {
+            @Override
+            public void onResponse(Call<ResponseModel> call, Response<ResponseModel> response) {
+                ResponseModel obj = response.body();
+                String result = obj.getMessage().trim();
+                if(result.equals("exists"))
+                {
+                    email_login.setText("");
+                    pass_login.setText("");
+                    Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_SHORT).show();
+
+                }
+                else if (response.equals("NotExist"))
+                {
+                    email_login.setText("");
+                    pass_login.setText("");
+                    Toast.makeText(MainActivity.this, "Envalid Username or Password!!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseModel> call, Throwable t) {
+                email_login.setText("");
+                pass_login.setText("");
+                Toast.makeText(MainActivity.this, "Login Failed!!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
