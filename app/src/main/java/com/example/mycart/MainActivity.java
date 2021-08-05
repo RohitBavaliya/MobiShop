5b7f3694fd4d5b78bf81fd452419f408a5f482ef
rohit <rohit13bavaliya@gmail.com>
2021-08-05 14:07:05 +0530

    Implementation of SharedPreference

--- app/src/main/java/com/example/mycart/MainActivity.java
+++ app/src/main/java/com/example/mycart/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mycart;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -27,6 +28,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
 
+        email_login = (EditText) findViewById(R.id.email_login);
+        pass_login = (EditText) findViewById(R.id.pass_login);
+        btn_login = (Button) findViewById(R.id.btn_login);
+        login_header = (TextView) findViewById(R.id.login_header);
+        login_result = (TextView) findViewById(R.id.login_result);
+
+        // check user already login or not
+        checkUserExist();
+
         go_register = (TextView) findViewById(R.id.go_register);
         go_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,11 +45,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        email_login = (EditText) findViewById(R.id.email_login);
-        pass_login = (EditText) findViewById(R.id.pass_login);
-        btn_login = (Button) findViewById(R.id.btn_login);
-        login_header = (TextView) findViewById(R.id.login_header);
-        login_result = (TextView) findViewById(R.id.login_result);
+
 
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +53,13 @@ public class MainActivity extends AppCompatActivity {
                 process_login(email_login.getText().toString(),pass_login.getText().toString());
             }
         });
+    }
+
+    // check user already login or not
+    private void checkUserExist() {
+        SharedPreferences sharedPreferences = getSharedPreferences("details_login",MODE_PRIVATE);
+        if(sharedPreferences.contains("email"))
+            startActivity(new Intent(MainActivity.this,Dashboard.class));
     }
 
     private void process_login(String email, String password)
@@ -64,10 +77,19 @@ public class MainActivity extends AppCompatActivity {
                 if(result.equals("Exist"))
                 {
                     login_header.setVisibility(View.INVISIBLE);
+
+                    SharedPreferences sharedPreferences = getSharedPreferences("details_login",MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("email",email);
+                    editor.putString("password",password);
+                    editor.commit();
+                    editor.apply();
+
                     login_result.setText("Login Successfull");
                     email_login.setText("");
                     pass_login.setText("");
                     startActivity(new Intent(getApplicationContext(),Dashboard.class));
+                    finish();
 
                 }
 
