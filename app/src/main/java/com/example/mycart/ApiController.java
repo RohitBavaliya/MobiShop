6b40d82b2298ccbc1f03ae0ef96054b2e6b94c00
rohit <rohit13bavaliya@gmail.com>
2021-10-06 20:43:03 +0530

    Api change

--- app/src/main/java/com/example/mycart/ApiController.java
+++ app/src/main/java/com/example/mycart/ApiController.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiController {
-    final static String url = "http://192.168.35.146/ecomapi/";
+    final static String url = "http://192.168.21.146/ecomapi/";
     private static ApiController controller;
     private static Retrofit retrofit;
 
